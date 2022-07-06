b0b6a9e190bbba5df61c5bf666d4212dc24277e1
Serhii Kravchenko <kravchenko087@ukr.net>
2022-07-06 23:11:18 +0300

    добавил компонент

---
+++ src/components/ReviewsVidget/ReviewsVidget.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class ReviewsVidget extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Please leave feadback</h2>
+                <button type='butoon'>Good</button>
+                <button type='butoon'>Neutural</button>
+                <button type='butoon'>Bad</button>
+            </div>
+        )
+    }
+}
+
+export default ReviewsVidget;
